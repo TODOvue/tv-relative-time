d13d363fafe35ea2acafe9b7196567ca2540a5c8
Cristhian Daza <cristian.d.b@hotmail.com>
2025-10-18 13:36:15 -0500

    fix(vite.config): update entry point to src/entry.ts and set exports to named

--- vite.config.js
+++ vite.config.js
@@ -20,7 +20,7 @@ export default defineConfig({
     }
     : {
       lib: {
-        entry: "src/components/TvRelativeTime.vue",
+        entry: "src/entry.ts",
         name: "TvRelativeTime",
         fileName: format => `tv-relative-time.${format}.js`,
         formats: ["es", "cjs"]
@@ -30,7 +30,8 @@ export default defineConfig({
         output: {
           globals: {
             vue: "Vue"
-          }
+          },
+          exports: 'named'
         }
       }
     },
