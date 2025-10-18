7a5ed108103680df4cdfd2720f1815b73440bde8
Cristhian Daza <cristian.d.b@hotmail.com>
2025-10-18 13:41:19 -0500

    feat(TvRelativeTime): add plugin installation method for component registration

--- src/main.js
+++ src/main.js
@@ -1,6 +1,4 @@
 import { createApp } from 'vue'
-import './style.css'
 import TvRelativeTime from './demo/Demo.vue'
-import 'github-markdown-css';
 
 createApp(TvRelativeTime).mount('#tv-relative-time')
