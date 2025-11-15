26ddc8e96749cc829d0cdd45deb8e524b3292f13
Cristhian Daza <cristian.d.b@hotmail.com>
2025-11-15 12:54:17 -0500

    refactor(TvRelativeTime): remove CSS runtime injection and migrate to extracted CSS build for SSR

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
+import { TvDemo } from '@todovue/tv-demo'
 import TvRelativeTime from './demo/Demo.vue'
+import '@todovue/tv-demo/style.css'
 
-createApp(TvRelativeTime).mount('#tv-relative-time')
+const app = createApp(TvRelativeTime)
+app.component('TvDemo', TvDemo)
+app.mount('#tv-relative-time')
