6f51357394fad0c209ac8d7ae3175eb4a9679e27
Cristhian Daza <cristian.d.b@hotmail.com>
2025-11-21 13:29:37 -0500

    feat(TvLabel): add Nuxt 4 integration and TypeScript support with new configuration files

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-<p align="center"><img width="150" src="https://firebasestorage.googleapis.com/v0/b/todovue-blog.appspot.com/o/logo.png?alt=media&token=d8eb592f-e4a9-4b02-8aff-62d337745f41" alt="TODOvue logo">
+<p align="center"><img width="150" src="https://res.cloudinary.com/dcdfhi8qz/image/upload/v1763663056/uqqtkgp1lg3xdplutpga.png" alt="TODOvue logo">
 </p>
 
 # TODOvue RelativeTime (TvRelativeTime)
--- src/entry.ts
+++ src/entry.ts
@@ -1,14 +1,20 @@
-import TvRelativeTime from './components/TvRelativeTime.vue'
+import type { App, Plugin } from 'vue'
+import _TvRelativeTime from './components/TvRelativeTime.vue'
 
-(TvRelativeTime as any).install = (app: any) => {
+const TvRelativeTime = _TvRelativeTime as typeof _TvRelativeTime & Plugin;
+TvRelativeTime.install = (app: App) => {
   app.component('TvRelativeTime', TvRelativeTime)
 };
 
-export const TvRelativeTimePlugin = {
-  install(app: any) {
-    app.component('TvRelativeTime', TvRelativeTime)
+export { TvRelativeTime }
+
+export const TvRelativeTimePlugin: Plugin = {
+  install: TvRelativeTime.install
+};
+export default TvRelativeTime;
+
+declare module 'vue' {
+  export interface GlobalComponents {
+    TvRelativeTime: typeof TvRelativeTime;
   }
 }
-
-export { TvRelativeTime }
-export default TvRelativeTime
